1b7f6426530605f0c8f942804e5a75bf87e99523
Md: Mohon <mdmohon6145@gmail.com>
2023-07-14 19:58:04 +0600

    user create rtk mutation added

---
+++ src/redux/user/userSlice.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { api } from "../api/apiSlice";
+
+const userApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    createUser: builder.mutation({
+      query: ({ data }) => ({
+        url: "user/signup",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useCreateUserMutation } = userApi;
